bcb625f177aa018a239a0428675e3097d3aed00d
Khushi <khushi.sharma_cs18@gla.ac.in>
2019-08-22 15:36:54 +0530

    create object of scanner class

--- src/main/main.java
+++ src/main/main.java
@@ -1,13 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author KHUSHI
  * @version 1.0
  */
 public class main {
     public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String userName = "";
-
+        scanner.close();
 
     }
 }
